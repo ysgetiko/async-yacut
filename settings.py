4918eff8d0218698ffb41720d8b6355b34323826
Evgeniy Getikov <zenia777@yandex.ru>
2026-03-23 22:19:17 +0700

    freeze

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SECRET_KEY = os.getenv("SECRET_KEY")
     DISK_TOKEN = os.getenv("DISK_TOKEN")
-    DISK_HOST = os.getenv("DISK_HOST")
+    DISK_HOST = 'https://cloud-api.yandex.net'
